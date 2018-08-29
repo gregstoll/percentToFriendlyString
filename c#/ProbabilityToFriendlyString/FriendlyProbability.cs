2c681c6670cd1b0b6edee16e27d5e411ca98fe48
Greg Stoll <greg@gregstoll.com>
2018-08-28 20:36:47 -0500

    Adding c# stub implementation with tests

---
+++ c#/ProbabilityToFriendlyString/FriendlyProbability.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Gregstoll.ProbabilityToFriendlyString
+{
+    public struct FriendlyProbability : IEquatable<FriendlyProbability>
+    {
+        public byte Numerator { get; }
+        public byte Denominator { get; }
+        public string FriendlyString { get; }
+
+        public FriendlyProbability(byte numerator, byte denominator, string friendlyString)
+        {
+            Numerator = numerator;
+            Denominator = denominator;
+            FriendlyString = friendlyString;
+        }
+
+        public FriendlyProbability(byte numerator, byte denominator) : this(numerator, denominator, string.Format("{0} in {1}", numerator, denominator))
+        {
+        }
+
+        public static FriendlyProbability FromProbability(double prob)
+        {
+            if (prob < 0 || prob > 1)
+            {
+                throw new ArgumentOutOfRangeException("probability must be between 0 and 1!");
+            }
+            if (prob == 0)
+            {
+                return new FriendlyProbability(0, 1);
+            }
+            if (prob == 1)
+            {
+                return new FriendlyProbability(1, 1);
+            }
+            return new FriendlyProbability(1, 2);
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is FriendlyProbability && Equals((FriendlyProbability)obj);
+        }
+
+        public bool Equals(FriendlyProbability other)
+        {
+            return Numerator == other.Numerator &&
+                   Denominator == other.Denominator &&
+                   FriendlyString == other.FriendlyString;
+        }
+
+        public override int GetHashCode()
+        {
+            var hashCode = 676277150;
+            hashCode = hashCode * -1521134295 + Numerator.GetHashCode();
+            hashCode = hashCode * -1521134295 + Denominator.GetHashCode();
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(FriendlyString);
+            return hashCode;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0}/{1} (text: \"{2}\")", Numerator, Denominator, FriendlyString);
+        }
+
+        public static bool operator ==(FriendlyProbability probability1, FriendlyProbability probability2)
+        {
+            return probability1.Equals(probability2);
+        }
+
+        public static bool operator !=(FriendlyProbability probability1, FriendlyProbability probability2)
+        {
+            return !(probability1 == probability2);
+        }
+    }
+}
---
+++ c#/ProbabilityToFriendlyStringTests/FriendlyProbabilityTests.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace Gregstoll.ProbabilityToFriendlyString
+{
+    [Serializable]
+    public class FriendlyProbabilityTests
+    {
+        public struct TestCase
+        {
+            public int LineNumber { get; }
+            public double Input { get; }
+            public FriendlyProbability Output { get; }
+            public TestCase(int lineNumber, double input, FriendlyProbability output)
+            {
+                LineNumber = lineNumber;
+                Input = input;
+                Output = output;
+            }
+        }
+        [Theory]
+        [MemberData(nameof(GetTestCases))]
+        public void TestsFromFile(TestCase testCase)
+        {
+            var actual = FriendlyProbability.FromProbability(testCase.Input);
+            if (testCase.Output != actual)
+            {
+                Assert.True(false, string.Format("expected {0} actual {1} from line {2}", testCase.Output, actual, testCase.LineNumber));
+            }
+        }
+
+        public static IEnumerable<object[]> GetTestCases()
+        {
+            List<TestCase> testCases = new List<TestCase>();
+            string path = Path.GetDirectoryName(typeof(FriendlyProbabilityTests).Assembly.Location);
+            string testCasesPath = null;
+            while (testCasesPath == null)
+            {
+                string tempPath = Path.Combine(path, "testCases.txt");
+                if (File.Exists(tempPath))
+                {
+                    testCasesPath = tempPath;
+                }
+                path = Path.GetDirectoryName(path);
+            }
+            var lines = File.ReadAllLines(testCasesPath);
+            int lineNumber = 1;
+            foreach (string rawLine in lines)
+            {
+                string line = rawLine.Trim();
+                if (line.StartsWith("#"))
+                {
+                    continue;
+                }
+                var parts = line.Split(',');
+                double input = double.Parse(parts[0]);
+                if (parts.Length == 3)
+                {
+                    testCases.Add(new TestCase(lineNumber, input, new FriendlyProbability(byte.Parse(parts[1]), byte.Parse(parts[2]))));
+                }
+                else if (parts.Length == 4)
+                {
+                    testCases.Add(new TestCase(lineNumber, input, new FriendlyProbability(byte.Parse(parts[1]), byte.Parse(parts[2]), parts[3])));
+                }
+                else
+                {
+                    throw new InvalidDataException(string.Format("line improperly formatted (line {0}: {1}", lineNumber, line));
+                }
+                ++lineNumber;
+            }
+
+            return testCases.Select(tc => new object[] { tc });
+        }
+    }
+}
