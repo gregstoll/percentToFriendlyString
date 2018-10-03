ee4dfad53a6d8dfd9e554c7894c1797f991144a0
Greg Stoll <greg@gregstoll.com>
2018-10-02 21:54:34 -0500

    finish writing tests with a stub implementation

--- java/src/com/gregstoll/probabilityToFriendlyString/FriendlyProbability.java
+++ java/src/com/gregstoll/probabilityToFriendlyString/FriendlyProbability.java
@@ -1,4 +1,61 @@
 package com.gregstoll.probabilityToFriendlyString;
+import java.util.Objects;
 
 public class FriendlyProbability {
+    private byte _numerator;
+    private byte _denominator;
+    private String _friendlyString;
+
+    public FriendlyProbability(byte numerator, byte denominator, String friendlyString)
+    {
+        _numerator = numerator;
+        _denominator = denominator;
+        _friendlyString = friendlyString;
+    }
+
+    public FriendlyProbability(byte numerator, byte denominator) {
+        this(numerator, denominator, String.format("%d in %d", numerator, denominator));
+    }
+
+    public static FriendlyProbability fromProbability(double prob) {
+        //TODO implement this
+        return new FriendlyProbability((byte)0, (byte)1);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null) {
+            return false;
+        }
+        if (getClass() != o.getClass()) {
+            return false;
+        }
+        FriendlyProbability otherProb = (FriendlyProbability)o;
+        return _numerator == otherProb._numerator && _denominator == otherProb._denominator && _friendlyString.equals(otherProb._friendlyString);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(_numerator, _denominator, _friendlyString);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%d/%d (text: \"%s\")", _numerator, _denominator, _friendlyString);
+    }
+
+    public byte getNumerator() {
+        return _numerator;
+    }
+
+    public byte getDenominator() {
+        return _denominator;
+    }
+
+    public String getFriendlyString() {
+        return _friendlyString;
+    }
 }
--- java/tests/com/gregstoll/probabilityToFriendlyString/Tests.java
+++ java/tests/com/gregstoll/probabilityToFriendlyString/Tests.java
@@ -2,10 +2,71 @@ package com.gregstoll.probabilityToFriendlyString;
 
 import static org.junit.Assert.assertEquals;
 import org.junit.Test;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
 
 public class Tests {
+    private class TestCase {
+        private int _lineNumber;
+        private double _input;
+        private FriendlyProbability _output;
+        public TestCase(int lineNumber, double input, FriendlyProbability output) {
+            _lineNumber = lineNumber;
+            _input = input;
+            _output = output;
+        }
+        public int getLineNumber() {
+            return _lineNumber;
+        }
+        public double getInput() {
+            return _input;
+        }
+        public FriendlyProbability getOutput() {
+            return _output;
+        }
+    }
+
     @Test
-    public void myFirstTest() {
-        assertEquals(2, 1+1);
+    public void testCases() throws FileNotFoundException, IOException {
+        ClassLoader classLoader = Tests.class.getClassLoader();
+        URL classLocation = classLoader.getResource("com/gregstoll/probabilityToFriendlyString/Tests.class");
+        File testCasesDirectory = new File(classLocation.getPath()).getParentFile();
+        while (!(new File(testCasesDirectory, "testCases.txt").exists())) {
+            testCasesDirectory = testCasesDirectory.getParentFile();
+        }
+        File testCasesFile = new File(testCasesDirectory, "testCases.txt");
+        FileReader testCasesFileReader = new FileReader(testCasesFile);
+        BufferedReader bufferedReader = new BufferedReader(testCasesFileReader);
+        
+        String line;
+        int lineNumber = 1;
+        ArrayList<TestCase> testCases = new ArrayList<TestCase>();
+        while((line = bufferedReader.readLine()) != null) {
+            if (line.startsWith("#")) {
+                continue;
+            }
+            String[] parts = line.split(",");
+            double input = Double.parseDouble(parts[0]);
+            if (parts.length == 3) {
+                testCases.add(new TestCase(lineNumber, input, new FriendlyProbability(Byte.parseByte(parts[1]), Byte.parseByte(parts[2]))));
+            }
+            else if (parts.length == 4) {
+                testCases.add(new TestCase(lineNumber, input, new FriendlyProbability(Byte.parseByte(parts[1]), Byte.parseByte(parts[2]), parts[3])));
+            }
+            else {
+                throw new IOException(String.format("line %d improperly formatted: %s", lineNumber, line));
+            }
+            lineNumber++;
+        }   
+        bufferedReader.close();
+        for (TestCase testCase : testCases) {
+            FriendlyProbability actual = FriendlyProbability.fromProbability(testCase.getInput());
+            assertEquals(String.format("line %d", testCase.getLineNumber()), testCase.getOutput(), actual);
+        }
     }
 }
