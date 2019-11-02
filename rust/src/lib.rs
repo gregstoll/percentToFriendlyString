47f6b6fd8df8c9168f9a63e6c4c8a969c004453f
Greg Stoll <greg@gregstoll.com>
2019-11-01 21:28:32 -0500

    added some methods in preparation for testing

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,4 +1,31 @@
+use std::fmt;
 
+#[derive(PartialEq, Eq, Debug)]
+pub struct FriendlyProbability {
+    numerator: u8,
+    denominator: u8,
+    friendly_string: String
+}
+
+impl fmt::Display for FriendlyProbability {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", self.friendly_string)
+    }
+}
+impl FriendlyProbability {
+    pub fn new(numerator: u8, denominator: u8) -> FriendlyProbability {
+        FriendlyProbability {
+            numerator: numerator,
+            denominator: denominator,
+            friendly_string: format!("{} in {}", numerator, denominator)
+        }
+    }
+
+    pub fn from_probability(probability: f32) -> FriendlyProbability {
+        // TODO
+        FriendlyProbability::new(0, 1)
+    }
+}
 
 #[cfg(test)]
 mod tests {
