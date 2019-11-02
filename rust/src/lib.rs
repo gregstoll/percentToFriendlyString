f96c8cad2a3d91c0b38d8a92ffe1981b403595c3
Greg Stoll <greg@gregstoll.com>
2019-11-01 21:58:33 -0500

    running tests, they all fail

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -29,8 +29,38 @@ impl FriendlyProbability {
 
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn all_cases() {
+        use std::fs;
+        let contents = fs::read_to_string("../testCases.txt").expect("Failed to open testCases.txt");
+        let lines = contents.lines();
+        let mut test_cases = 0;
+        for line in lines {
+            if line.starts_with("#") {
+                continue;
+            }
+            let parts : Vec<&str> = line.trim().split(",").collect();
+            let numerator = parts[1].parse::<u8>().expect("parsing numerator");
+            let denominator = parts[2].parse::<u8>().expect("parsing denominator");
+            let expected : FriendlyProbability;
+            if parts.len() == 3 {
+                expected = FriendlyProbability::new(numerator, denominator);
+            } else if parts.len() == 4 {
+                expected = FriendlyProbability {
+                    numerator: numerator,
+                    denominator: denominator,
+                    friendly_string: parts[3].to_string()
+                 };
+            } else {
+                panic!(format!("Got string with wrong number of parts: {}", line.trim()));
+            }
+            let probability = parts[0].parse::<f32>().expect("parsing probability");
+            assert_eq!(FriendlyProbability::from_probability(probability), expected);
+            test_cases += 1;
+        }
+        println!("Ran {} test cases", test_cases);
+        assert!(test_cases > 0);
     }
 }
