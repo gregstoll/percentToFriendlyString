7202cc151cc9043d61b178007d3a0a542ff9943d
Greg Stoll <greg@gregstoll.com>
2019-11-01 22:22:43 -0500

    starting to set up fractions

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,4 +1,6 @@
 use std::fmt;
+#[macro_use]
+extern crate lazy_static;
 
 #[derive(PartialEq, Eq, Debug)]
 pub struct FriendlyProbability {
@@ -25,6 +27,36 @@ impl FriendlyProbability {
         // TODO
         FriendlyProbability::new(0, 1)
     }
+}
+
+// TODO - use slice::binary_search_by on these since we can't derive Ord
+// because f32's are not orderable
+// https://stackoverflow.com/questions/28247990/how-to-do-a-binary-search-on-a-vec-of-floats
+#[derive(PartialOrd, PartialEq)]
+struct Fraction {
+    value: f32,
+    numerator: u8,
+    denominator: u8,
+}
+impl Fraction {
+    pub fn new(numerator: u8, denominator: u8) -> Fraction {
+        Fraction {
+            numerator: numerator,
+            denominator: denominator,
+            value: numerator as f32/denominator as f32
+        }
+    }
+}
+
+lazy_static! {
+    static ref FRACTION_DATA: Vec<Fraction> = {
+        let mut d : Vec<Fraction> = Vec::new();
+        d.push(Fraction::new(1, 2));
+        // TODO - more fractions
+        // TODO - make sure this is in ascending order
+        d.sort_unstable_by(|a,b| a.partial_cmp(b).unwrap());
+        d
+    };
 }
 
 #[cfg(test)]
