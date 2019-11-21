90e3d1bf230d7b7e2f715966d6fc8cd97384af78
Greg Stoll <greg@gregstoll.com>
2019-11-20 21:04:42 -0600

    Add TypeScript definitiom file

---
+++ javascript/probabilityToFriendlyString.d.ts
@@ -0,0 +1,12 @@
+class FriendlyProbability {
+    constructor(numerator: number, denominator: number, friendlyString: string);
+
+    numerator: number;
+    denominator: number;
+    friendlyString: string;
+
+    toString(): string;
+    equals(other: FriendlyProbability): boolean;
+    static fromProbability(f: number): FriendlyProbability;
+}
+export = FriendlyProbability;
